b94a257d68b9ef2b9d23f00be8844668b57e0b2e
sonyaq <sonazubrilova@gmail.com>
2026-04-13 21:20:55 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,131 @@
+
+//1
+const user = {
+  hobby: "dance",
+  premium: true,
+};
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = false;
+const keyUser = Object.keys(user);
+for (const key of keyUser) {
+  console.log(`${key}: ${user[key]}`);
+}
+
+//2
+console.log("Завдання 2");
+function countProps(obj) {
+  let result = Object.keys(obj).length;
+  return result;
+}
+console.log(countProps(user));
+
+//3
+console.log("Завдання 3");
+const employees = {
+  Artem: 5,
+  Yaroslav: 9,
+  Sonya: 11,
+  Danil: 2,
+  Andrey: 4,
+};
+function findBestEmployee(employees) {
+  let result = 0;
+  for (const employee in employees) {
+    if (employees[employee] > result) {
+      result = employees[employee];
+    }
+  }
+  return result;
+}
+console.log(findBestEmployee(employees));
+
+//4
+console.log('Завдання 4');
+const salaryEmployees = {
+  Artem: 5500,
+  Yaroslav: 9000,
+  Sonya: 11000,
+  Danil: 2100,
+  Andrey: 4700,
+};
+function countTotalSalary(employees) {
+  let sum = 0;
+  for (const employee in employees) {
+    sum += employees[employee];
+  }
+  return sum;
+}
+console.log(countTotalSalary(salaryEmployees));
+
+//5
+console.log('Завдання 5');
+const bricks = [
+  {
+    model: "C1005",
+    color: "pink",
+    details: 205,
+  },
+  {
+    model: "C1010",
+    color: "orange",
+    details: 172,
+  },
+  {
+    model: "C1006",
+    color: "purple",
+    details: 210,
+  },
+];
+
+function getAllPropValues(arr, prop) {
+   const result = [];
+  for (const bricks of arr) {
+      if (bricks[prop ] !== undefined) {
+         result.push(bricks[prop]) 
+     }
+  }
+  return result;
+}
+console.log(getAllPropValues(bricks, "color"));
+
+//6
+console.log('Завдання 6');
+const products = [
+  {
+    name: "Orange".toLowerCase(),
+    price: 12,
+    quantity: 50,
+  },
+  {
+    name: "Coffe".toLowerCase(),
+    price: 35,
+    quantity: 30,
+  },
+  {
+    name: "Bars".toLowerCase(),
+    price: 10,
+    quantity: 100,
+  },
+  {
+    name: "ice cream".toLowerCase(),
+    price: 22,
+    quantity: 20,
+  },
+];
+
+function calculateTotalPrice(allProdcuts, productName) {
+  let sum = 0;
+  for (const product of allProdcuts) {
+      if (product.name === productName) {
+         sum = product.price * product.quantity
+     }
+  }
+  return sum;
+}
+ console.log(calculateTotalPrice(products, "coffe".toLowerCase()));
+
+
 const Transaction = {
   DEPOSIT: 'deposit',
   WITHDRAW: 'withdraw',
@@ -6,8 +134,8 @@ const account = {
     balance: 0,
     transactions: [],
   createTransaction(amount, type) {
-      this.transactions.push({amount: amount, typeTransaction: type }); 
-     
+      this.transactions.push({id: this.transactions.length + 1,amount: amount, typeTransaction: type }); 
+
     },
   deposit(amount) {
   this.balance += amount;
@@ -44,6 +172,6 @@ console.log(account.withdraw(100));
 console.log(account.deposit(700));
 console.log(account.getBalance());
 console.log(account.transactions);
-console.log(account.getTransactionDetails(2));
+console.log(account.getTransactionDetails(Number(prompt('Введіть айди транзакціїї'))));
 console.log(account.getTransactionTotal(Transaction.DEPOSIT));
 
